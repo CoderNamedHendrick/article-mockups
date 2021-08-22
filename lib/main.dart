72a41fb1b7546d3febfe30ddc5567f91e6f893fe
Hendrick <sebastinesoacatp@gmail.com>
2021-08-22 12:23:37 +0100

    done with performing splashscreens with animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:animated_splash_screen/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([
+    SystemUiOverlay.bottom,
+    SystemUiOverlay.top,
+  ]);
   runApp(MyApp());
 }
 
@@ -11,9 +17,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'animated_splash_screen',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: SplashScreen(),
     );
   }
@@ -32,36 +35,66 @@ class _SplashScreenState extends State<SplashScreen>
   void initState() {
     super.initState();
     _splashController = AnimationController(
-        vsync: this,
-        duration: Duration(
-          milliseconds: 650,
-        ));
+      vsync: this,
+      duration: Duration(
+        milliseconds: 1000,
+      ),
+    );
     WidgetsBinding.instance.addPostFrameCallback((_) => animateToHome(context));
   }
 
   Future<void> animateToHome(BuildContext context) async {
+    await Future.delayed(Duration(milliseconds: 500));
     await _splashController.forward();
     await _splashController.reverse();
-    Navigator.of(context).pushReplacement(MaterialPageRoute(
-      builder: (context) => Home(),
-    ));
+    Navigator.of(context).pushReplacement(_splashRoute());
+  }
+
+  Route _splashRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => const Home(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var curve = Curves.ease;
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        final tween = Tween(begin: begin, end: end);
+        final curvedAnimation =
+            CurvedAnimation(parent: animation, curve: curve);
+        return SlideTransition(
+          position: tween.animate(curvedAnimation),
+          child: child,
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    final scheme = MediaQuery.of(context).platformBrightness;
+    bool darkMode = scheme == Brightness.dark;
+    final tween = Tween<double>(begin: 250, end: 500);
+    var animation = tween.animate(_splashController);
     return Scaffold(
       body: Container(
-        color: Colors.black54,
-        child: Center(
+        color: darkMode ? Colors.black : Colors.white,
+        child: Align(
+          alignment: Alignment.center,
           child: RotationTransition(
             turns: _splashController,
-            child: Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
+            child: AnimatedBuilder(
+              animation: _splashController,
+              builder: (context, child) {
+                return Container(
+                  height: animation.value,
+                  width: animation.value,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
                       fit: BoxFit.fill,
-                      image: AssetImage('assets/images/twitter_logo.png'))),
+                      image: AssetImage('assets/images/splash.png'),
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ),
